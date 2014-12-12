3996da547cc9a11d26f3b5601de4be289683fb33
alexkappa <alex.kalyvitis@gmail.com>
2014-12-12 15:04:24 +0100

    first attempt at displaying the whole directory in a single page

--- dc.go
+++ dc.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/hoisie/mustache"
 	"github.com/skratchdot/open-golang/open"
@@ -40,17 +43,17 @@ func main() {
 		os.Exit(1)
 	}
 	if serve {
-		go open.Run("http://" + addr)
+		go open.Run("http://" + addr + "/render")
 		http.ListenAndServe(addr, handle(html, data))
 	} else {
 		t, err := parseTemplate(html)
 		if err != nil {
-			fmt.Fprintf(os.Stdout, "Unable to parse template. %s\n", err)
+			fmt.Fprintf(os.Stderr, "Unable to parse template. %s\n", err)
 			os.Exit(1)
 		}
 		d, err := parseData(data)
 		if err != nil {
-			fmt.Fprintf(os.Stdout, "Unable to parse data. %s\n", err)
+			fmt.Fprintf(os.Stderr, "Unable to parse data. %s\n", err)
 			os.Exit(1)
 		}
 		fmt.Println(t.Render(d))
@@ -59,7 +62,14 @@ func main() {
 
 func parseTemplate(s string) (*mustache.Template, error) {
 	if f, err := os.Open(s); err == nil {
-		f.Close()
+		defer f.Close()
+		finfo, err := f.Stat()
+		if err != nil {
+			return nil, err
+		}
+		if finfo.IsDir() {
+			return parseTemplateDir(s)
+		}
 		return mustache.ParseFile(s)
 	} else if r, err := http.Get(s); err == nil {
 		b, err := ioutil.ReadAll(r.Body)
@@ -70,6 +80,42 @@ func parseTemplate(s string) (*mustache.Template, error) {
 		return mustache.ParseString(string(b))
 	}
 	return mustache.ParseString(s)
+}
+
+func parseTemplateDir(dir string) (*mustache.Template, error) {
+	d, err := os.Open(dir)
+	if err != nil {
+		fmt.Println(dir, err)
+		return nil, err
+	}
+	defer d.Close()
+	files, err := d.Readdirnames(0)
+	if err != nil {
+		return nil, err
+	}
+	var t, p string
+	for _, file := range files {
+		f, err := os.Open(filepath.Join(dir, file))
+		if err != nil {
+			return nil, err
+		}
+		b, err := ioutil.ReadAll(f)
+		if err != nil {
+			return nil, err
+		}
+		dim := strings.Split(file[:strings.Index(file, ".")], "x")
+		if len(dim) != 2 {
+			return nil, errors.New("file names should be in the form {width}x{height}.html")
+		}
+		r := strings.NewReplacer(
+			"{{name}}", file,
+			"{{content}}", string(b),
+			"{{width}}", dim[0],
+			"{{height}}", dim[1])
+		p += r.Replace(frame)
+	}
+	t = strings.Replace(layout, "{{frames}}", p, 1)
+	return mustache.ParseString(t)
 }
 
 func parseData(s string) (v interface{}, err error) {
@@ -114,3 +160,46 @@ func handle(html, data string) http.HandlerFunc {
 		fmt.Fprintf(w, "%s", t.Render(d))
 	}
 }
+
+const layout = `<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        * {
+            font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;
+        }
+        h2 {
+            margin: 5px 0;
+        }
+        .note {
+            display: block;
+            text-align: center;
+            background: #f0f0f0;
+            line-height: 36px;
+        }
+        div {
+            float: left;
+            margin: 15px;
+        }
+        .long {
+            height: 200px;
+        }
+        .square {
+            width: 50%;
+            float: left;
+            margin-top: -5%;
+        }
+    </style>
+</head>
+<body>
+    <p class="note">Please clean cache browser before reviewing it. Reload holding shift key</p>
+    {{frames}}
+</body>
+</html>`
+
+const frame = `<div>
+	<h2>{{name}}</h2>
+	<iframe src="" width="{{width}}px" height="{{height}}px" scrolling="no" frameborder="0">
+		{{content}}
+	</iframe>
+</div>`
