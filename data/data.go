5e320842e944b5860154e80ffe78b9055a1bdb7b
alexkappa <alex.kalyvitis@gmail.com>
2015-01-11 17:24:35 +0100

    fixed subtle bug where readers wouldn't get closes properly

--- data/data.go
+++ data/data.go
@@ -13,18 +13,18 @@ import (
 // that fails it treats s as input and attempts to parse it.
 func Parse(s string) (interface{}, error) {
 	if f, err := os.Open(s); err == nil {
+		defer f.Close()
 		b, err := ioutil.ReadAll(f)
 		if err != nil {
 			return nil, err
 		}
-		defer f.Close()
 		return unmarshal(b)
 	} else if r, err := http.Get(s); err == nil {
+		defer r.Body.Close()
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			return nil, err
 		}
-		r.Body.Close()
 		return unmarshal(b)
 	}
 	return unmarshal([]byte(s))
